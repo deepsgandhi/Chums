eac7154bc104108b66017c02eaf7314200ea2e3f
Sybil Jas <51464288+sybiljas@users.noreply.github.com>
2020-10-31 09:45:56 +0530

    Issue #55 code update (#57)
    * added coded for edit/delete note
    
    * removed duplicate values

--- ChumsApp/src/Components/Notes.tsx
+++ ChumsApp/src/Components/Notes.tsx
@@ -1,32 +1,77 @@
-import React from 'react';
-import { ApiHelper, Note, DisplayBox, InputBox, UserHelper } from './';
+import React from "react";
+import { NoteInterface } from "../Utils";
+import { ApiHelper, Note, DisplayBox, InputBox, UserHelper } from "./";
 
-interface Props { contentId: number, contentType: string }
+interface Props {
+  contentId: number;
+  contentType: string;
+}
 
 export const Notes: React.FC<Props> = (props) => {
-    const [notes, setNotes] = React.useState([]);
-    const [noteText, setNoteText] = React.useState('');
+  const [notes, setNotes] = React.useState([]);
+  const [noteText, setNoteText] = React.useState("");
 
-    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setNoteText(e.currentTarget.value);
-    const loadNotes = () => { if (props.contentId > 0) ApiHelper.apiGet('/notes/' + props.contentType + '/' + props.contentId).then(data => setNotes(data)); }
-    const handleSave = () => {
-        var n = { contentId: props.contentId, contentType: props.contentType, contents: noteText }
-        ApiHelper.apiPost('/notes', [n]).then(() => { loadNotes(); setNoteText(''); });
-    }
-    React.useEffect(loadNotes, [props.contentId]);
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setNoteText(e.currentTarget.value);
+  const loadNotes = () => {
+    if (props.contentId > 0)
+      ApiHelper.apiGet(
+        "/notes/" + props.contentType + "/" + props.contentId
+      ).then((data) => setNotes(data));
+  };
+  const handleSave = () => {
+    var n = {
+      contentId: props.contentId,
+      contentType: props.contentType,
+      contents: noteText,
+    };
+    ApiHelper.apiPost("/notes", [n]).then(() => {
+      loadNotes();
+      setNoteText("");
+    });
+  };
 
-    var noteArray: React.ReactNode[] = [];
-    for (var i = 0; i < notes.length; i++) noteArray.push(<Note note={notes[i]} key={notes[i].id} />);
+  React.useEffect(loadNotes, [props.contentId]);
 
-    var canEdit = UserHelper.checkAccess('People', 'Edit Notes')
-    if (!canEdit) return <DisplayBox headerIcon="far fa-sticky-note" headerText="Notes" >{noteArray}</DisplayBox>
-    else return (
-        <InputBox id="notesBox" headerIcon="far fa-sticky-note" headerText="Notes" saveFunction={handleSave} saveText="Add Note" >
-            {noteArray}<br />
-            <div className="form-group">
-                <label>Add a Note</label>
-                <textarea id="noteText" className="form-control" name="contents" onChange={handleChange} value={noteText} />
-            </div>
-        </InputBox>
-    )
-}
+  const handleDelete = (noteId: number) => () => {
+    ApiHelper.apiDelete(`/notes/${noteId}`);
+    setNotes(notes.filter((note) => note.id !== noteId));
+  };
+
+  var noteArray: React.ReactNode[] = [];
+  for (var i = 0; i < notes.length; i++)
+    noteArray.push(
+      <Note note={notes[i]} key={notes[i].id} handleDelete={handleDelete} />
+    );
+
+  var canEdit = UserHelper.checkAccess("People", "Edit Notes");
+  if (!canEdit)
+    return (
+      <DisplayBox headerIcon="far fa-sticky-note" headerText="Notes">
+        {noteArray}
+      </DisplayBox>
+    );
+  else
+    return (
+      <InputBox
+        id="notesBox"
+        headerIcon="far fa-sticky-note"
+        headerText="Notes"
+        saveFunction={handleSave}
+        saveText="Add Note"
+      >
+        {noteArray}
+        <br />
+        <div className="form-group">
+          <label>Add a Note</label>
+          <textarea
+            id="noteText"
+            className="form-control"
+            name="contents"
+            onChange={handleChange}
+            value={noteText}
+          />
+        </div>
+      </InputBox>
+    );
+};
