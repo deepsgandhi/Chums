06332bf52c328137e14252b848aa4853d35a3089
pranavmalvawala <pranavmalvawala@gmail.com>
2020-11-14 12:17:14 +0530

    add modal: Show update address modal when add existing member to household

--- ChumsApp/src/Components/PersonAdd.tsx
+++ ChumsApp/src/Components/PersonAdd.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { ApiHelper, PersonInterface, PersonHelper } from './';
 import { Table, Button, FormControl, InputGroup } from 'react-bootstrap';
+import { UpdateHouseHold } from '../People/Components'
 
-interface Props { addFunction: (person: PersonInterface) => void }
+interface Props { addFunction: (person: PersonInterface) => void, person? : PersonInterface }
 
 export const PersonAdd: React.FC<Props> = (props) => {
-    const [searchResults, setSearchResults] = React.useState(null);
+    const [searchResults, setSearchResults] = React.useState<PersonInterface[]>(null);
     const [searchText, setSearchText] = React.useState('');
+    const [showUpdateAddressModal, setShowUpdateAddressModal] = React.useState<boolean>(false)
+    const [text, setText] = React.useState('');
+    const [selectedPersonIndex, setSelectedPersonIndex] = React.useState<number>(null)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => { e.preventDefault(); setSearchText(e.currentTarget.value); }
     const handleKeyDown = (e: React.KeyboardEvent<any>) => { if (e.key === 'Enter') { e.preventDefault(); handleSearch(null); } }
@@ -16,12 +20,40 @@ export const PersonAdd: React.FC<Props> = (props) => {
     }
     const handleAdd = (e: React.MouseEvent) => {
         e.preventDefault();
+
         var anchor = e.currentTarget as HTMLAnchorElement;
         var idx = anchor.getAttribute('data-index');
-        var sr = searchResults;
-        var person = sr.splice(idx, 1)[0];
-        setSearchResults(sr);
+        var sr: PersonInterface[] = [...searchResults];       
+        var person: PersonInterface = sr.splice(parseInt(idx), 1)[0];
+        setSelectedPersonIndex(parseInt(idx))
+        // console.log('Show modal here!!', props.person)
+        const {name: {
+            first
+        }, contactInfo: {
+            address1, address2, city, state, zip
+        }} = props.person
+        setText(`Would you like to update ${person.name.first}'s address to match ${first}'s (${address1} ${address2} ${city} ${state} ${zip})?`)
+        setShowUpdateAddressModal(true)                
+    }
+
+    const addPerson = () => {
+        const updatedSearch: PersonInterface[] = [...searchResults]       
+        const person: PersonInterface = updatedSearch.splice(selectedPersonIndex, 1)[0];
+      
+        setSearchResults(updatedSearch);
         props.addFunction(person);
+    }
+
+    const handleNo = () => {
+        setShowUpdateAddressModal(false);
+        addPerson()
+    }
+
+    const handleYes = () => {
+        setShowUpdateAddressModal(false)
+        // TODO: create an API/ Check if already present, to support this feature
+        console.log('Yes! update my address')
+        addPerson()
     }
 
     var rows = [];
@@ -32,7 +64,7 @@ export const PersonAdd: React.FC<Props> = (props) => {
                 <tr key={sr.id}>
                     <td><img src={PersonHelper.getPhotoUrl(sr)} alt="avatar" /></td>
                     <td>{sr.name.display}</td>
-                    <td><a href="about:blank" className="text-success" data-index={i} onClick={handleAdd}><i className="fas fa-user"></i> Add</a></td>
+                    <td><a className="text-success" data-index={i} onClick={handleAdd}><i className="fas fa-user"></i> Add</a></td>
                 </tr>
             );
         }
@@ -40,6 +72,7 @@ export const PersonAdd: React.FC<Props> = (props) => {
 
     return (
         <>
+            <UpdateHouseHold show={showUpdateAddressModal} onHide={() => setShowUpdateAddressModal(false)} handleNo={handleNo} handleYes={handleYes} text={text} />
             <InputGroup>
                 <FormControl id="personAddText" value={searchText} onChange={handleChange} onKeyDown={handleKeyDown} />
                 <div className="input-group-append"><Button id="personAddButton" variant="primary" onClick={handleSearch} ><i className="fas fa-search"></i> Search</Button></div>
