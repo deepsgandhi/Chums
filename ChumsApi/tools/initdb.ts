0992d4e47cebdba34390283939e88614122c25a5
Jeremy Zongker <jeremy@trilitech.com>
2021-01-15 01:21:25 -0600

    Cleaned up initdb

--- ChumsApi/tools/initdb.ts
+++ ChumsApi/tools/initdb.ts
@@ -1,37 +1,65 @@
 import dotenv from "dotenv";
-import fs from "fs-extra";
-import mysql from "mysql";
-import util from "util";
+import { DB } from "../src/apiBase/db";
+import { Pool } from "../src/apiBase/pool";
+import { DBCreator } from "../src/apiBase/tools/DBCreator"
 
 const init = async () => {
   dotenv.config();
-  const conn = mysql.createConnection({ host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, debug: true });
-  const query = util.promisify(conn.query).bind(conn);
+  console.log("Connecting");
+  Pool.initPool();
 
-  try {
-    await runFile(query, "./tools/initdb.mysql");
-    await runFile(query, "./src/apiBase/tools/initdb.mysql");
-    await runFile(query, "./tools/reports.mysql");
-  } catch (err) {
-    console.log(err); throw err;
-  } finally { conn.end(); }
+  const peopleTables: { title: string, file: string }[] = [
+    { title: "Households", file: "households.mysql" },
+    { title: "People", file: "people.mysql" },
+  ]
 
-};
+  const groupTables: { title: string, file: string }[] = [
+    { title: "Campuses", file: "campuses.mysql" },
+    { title: "Services", file: "services.mysql" },
+    { title: "Service Times", file: "serviceTimes.mysql" },
+    { title: "Groups", file: "groups.mysql" },
+    { title: "Group Service Times", file: "groupServiceTimes.mysql" },
+    { title: "Group Members", file: "groupMembers.mysql" },
+  ];
 
-const runFile = async (query: any, fileName: string) => {
-  try {
-    var sql = await fs.readFile(fileName, { encoding: "UTF-8" });
-    const statements = sql.split(/;(?=END)\s*$|;(?!\nEND)\s*$/gm);
-    for (const statement of statements) {
-      if (statement.length > 3) {
-        console.log("*************");
-        console.log(statement);
-        await query(statement);
-      }
-    }
-  } catch (err) { console.log(err); throw err; }
+  const attendanceTables: { title: string, file: string }[] = [
+    { title: "Sessions", file: "sessions.mysql" },
+    { title: "Visits", file: "visits.mysql" },
+    { title: "Visit Sessions", file: "visitSessions.mysql" }
+  ]
+
+  const givingTables: { title: string, file: string }[] = [
+    { title: "Funds", file: "funds.mysql" },
+    { title: "Donations", file: "donations.mysql" },
+    { title: "Fund Donations", file: "funddonations.mysql" },
+    { title: "Donation Batches", file: "donationBatches.mysql" },
+  ];
+
+  const miscTables: { title: string, file: string }[] = [
+    { title: "Church Settings", file: "churchsettings.mysql" },
+    { title: "Reports", file: "reports.mysql" },
+  ]
+
+  await DBCreator.init(["Answers", "Forms", "FormSubmissions", "Notes", "Questions"]);
+  await initTables("People", peopleTables);
+  await initTables("Groups", groupTables);
+  await initTables("Attendance", attendanceTables);
+  await initTables("Giving", givingTables);
+  await initTables("Misc", miscTables);
+  await DBCreator.runScript("Populate Reports", "./tools/dbScripts/populateReports.mysql", true);
+}
+
+const initTables = async (displayName: string, tables: { title: string, file: string }[]) => {
+  console.log("");
+  console.log("SECTION: " + displayName);
+  for (const table of tables) await DBCreator.runScript(table.title, "./tools/dbScripts/" + table.file, false);
 }
 
 init()
-  .then(() => { console.log("Database Created"); })
-  .catch(() => { console.log("Database not created due to errors"); });
+  .then(() => { console.log("Database Created"); process.exit(0); })
+  .catch((ex) => {
+    console.log(ex);
+    console.log("Database not created due to errors");
+    process.exit(0);
+  });
+
